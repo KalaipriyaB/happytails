699074cfbacb04f118c6ce2d9afad4f5857f8553
KalaipriyaB <80263143+KalaipriyaB@users.noreply.github.com>
2021-03-09 15:40:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS.css"> 
+    <script src="https://use.fontawesome.com/26ab819582.js"></script>
+  </head>
+  
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="title">Happy Tails</h1>
+      </div>
+    </div>
+    <div class="read-me">
+        <p >Animals have no voice of their own, no social safety net and very few rights under the law.<br>They are vulnerable and unfortunately mistreated more often than most of us would like to think. <br>Shelters and rescue operations for pets need donations from the community to operate.
+        </p>
+        <a class="btn btn-grad" href="https://rzp.io/l/gcVVXYa" role="button">DONATE <i class="fa fa-gift" aria-hidden="true"></i></a>
+    </div>
+      
+    <div class="donate-img">
+        <img src="donate.png">
+    </div>
+            
+    <div class="quote">
+          <p>We can make a Life by giving..<br>So let's start donating today &#9829;</p>
+    </div>
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+  </body>
+</html>
